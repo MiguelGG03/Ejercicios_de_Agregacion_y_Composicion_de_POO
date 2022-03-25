d91f50df2c59194efd80faacb6485073cc6dd2a1
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-25 12:26:48 +0100

    cambios en el main, plantilla para elegir EJ

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 def main():
-    print()
-    
+    acabar=False
+    while(acabar==False):
+        seguir=str(input('Quieres seguir viendo ejercicios(S/N)?:'))    
+        if(seguir=='s' or seguir=='S'):
+            cual=str(input('Que ejercicio quieres ver (4,5,6)?:'))
+            if(cual=='a' or cual=='A'):
+                print('Ejercicio A:\n')
+                
+            elif(cual=='b' or cual=='B'):
+                print('Ejercicio B:\n')
+                
+            elif(cual=='c' or cual=='C'):
+                print('Ejercicio C:\n')
+            else:
+                print('Eso no es una A,B o C.')
+        if(seguir=='n' or seguir=='N'):
+            acabar=True
+        else:
+            print('Eso no es una S o una N')
+
 if __name__=='__name__':
     main()
